1b2fdb7cb8b2aeece8fee04d6f7a140c40bef4b6
molpopgen <krthornt@uci.edu>
2022-02-25 09:54:17 -0800

    more stuff we may need

--- ancestry-inline-simplification-prototype/src/ancestry.rs
+++ ancestry-inline-simplification-prototype/src/ancestry.rs
@@ -1,5 +1,5 @@
 use crate::{LargeSignedInteger, Segment, SignedInteger};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Clone, Copy)]
 pub enum NodeStatus {
@@ -9,6 +9,7 @@ pub enum NodeStatus {
 
 pub struct Ancestry {
     next_node_id: SignedInteger,
+    node_to_index: HashMap<SignedInteger, usize>,
     pub status: Vec<NodeStatus>,
     pub ancestry: Vec<Vec<Segment>>,
     pub children: Vec<Vec<Segment>>,
@@ -22,8 +23,15 @@ impl Ancestry {
     pub fn new(num_nodes: SignedInteger) -> Self {
         assert!(num_nodes > 0);
 
+        let mut node_to_index = HashMap::<SignedInteger, usize>::default();
+
+        for i in 0..num_nodes {
+            node_to_index.insert(i, i as usize);
+        }
+
         Self {
             next_node_id: num_nodes,
+            node_to_index,
             status: vec![NodeStatus::ALIVE; num_nodes as usize],
             ancestry: vec![vec![]; num_nodes as usize],
             children: vec![vec![]; num_nodes as usize],
