6abc8c77aafcae532fd373c7fac6638143ddc9c1
molpopgen <krthornt@uci.edu>
2022-02-25 12:13:52 -0800

    some more infrastructure

--- ancestry-inline-simplification-prototype/src/simulate.rs
+++ ancestry-inline-simplification-prototype/src/simulate.rs
@@ -4,6 +4,19 @@ use crate::LargeSignedInteger;
 use crate::SignedInteger;
 use rgsl::rng;
 
+fn ran_flat(rng: &mut rng::Rng, lo: f64, hi: f64) -> f64 {
+    let mut rv = rng.flat(lo, hi);
+
+    while match rv.partial_cmp(&hi) {
+        Some(std::cmp::Ordering::Equal) => true,
+        Some(_) => false,
+        None => panic!("gsl_rng_ran_flat should not return NaN"),
+    } {
+        rv = rng.flat(lo, hi);
+    }
+    rv
+}
+
 pub fn simulate(
     seed: usize,
     popsize: SignedInteger,
