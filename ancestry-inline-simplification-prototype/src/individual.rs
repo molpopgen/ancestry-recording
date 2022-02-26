e80a5d26d2e24131558c61d5dcb698f5a5e57bb7
molpopgen <krthornt@uci.edu>
2022-02-26 14:50:11 -0800

    newtypes make things better...

--- ancestry-inline-simplification-prototype/src/individual.rs
+++ ancestry-inline-simplification-prototype/src/individual.rs
@@ -1,6 +1,7 @@
 use crate::{LargeSignedInteger, Segment, SignedInteger};
-use std::cell::RefCell;
+use std::hash::{Hash, Hasher};
 use std::rc::Rc;
+use std::{cell::RefCell, ops::Deref};
 
 // Use this over std::collections b/c the hashing
 // fn is much faster. (We aren't doing cryptography.)
@@ -8,15 +9,53 @@ use std::rc::Rc;
 // they recommend here.
 use hashbrown::{HashMap, HashSet};
 
-pub type ChildMap = HashMap<SignedInteger, Vec<Segment>>;
+/// Pointer to an individual.
+///
+/// Derefs to Rc<RefCell<Individual>>.
+/// Required so that we can hash Rc instances.
+#[derive(Clone)]
+pub struct IndividualPointer(Rc<RefCell<Individual>>);
+
+pub type ChildMap = HashMap<IndividualPointer, Vec<Segment>>;
+pub type ParentSet = HashSet<IndividualPointer>;
 
 #[derive(Clone)]
 pub struct Individual {
     index: SignedInteger,
     alive: bool,
-    parents: Vec<IndividualPointer>,
+    parents: ParentSet,
     ancestry: Vec<Segment>,
     children: ChildMap,
+}
+
+impl Deref for IndividualPointer {
+    type Target = Rc<RefCell<Individual>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl PartialEq for IndividualPointer {
+    fn eq(&self, other: &Self) -> bool {
+        self.borrow().index == other.borrow().index
+    }
+}
+
+impl Eq for IndividualPointer {}
+
+impl Hash for IndividualPointer {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.borrow().index.hash(state);
+    }
+}
+
+impl IndividualPointer {
+    pub fn new(index: SignedInteger) -> Self {
+        Self {
+            0: Rc::new(RefCell::<Individual>::new(Individual::new(index))),
+        }
+    }
 }
 
 impl Individual {
@@ -24,14 +63,12 @@ impl Individual {
         Self {
             index,
             alive: true,
-            parents: vec![],
+            parents: ParentSet::default(),
             ancestry: vec![],
             children: ChildMap::default(),
         }
     }
 }
-
-type IndividualPointer = Rc<RefCell<Individual>>;
 
 // This module is for experimenting with the Rc/RefCell pattern.
 #[cfg(test)]
@@ -39,94 +76,79 @@ mod practice_tests {
     use super::*;
 
     fn remove_parent(parent: IndividualPointer, child: IndividualPointer) {
-        child
-            .borrow_mut()
-            .parents
-            .retain(|x| x.borrow().index != parent.borrow().index);
+        child.borrow_mut().parents.remove(&parent);
     }
 
     // Better -- does not increase ref counts just for fn call.
     fn remove_parent_via_ref(parent: &IndividualPointer, child: &IndividualPointer) {
-        child
-            .borrow_mut()
-            .parents
-            .retain(|x| x.borrow().index != parent.borrow().index);
+        child.borrow_mut().parents.remove(&parent);
     }
 
     #[test]
     fn test_interior_mutability() {
         let mut pop: Vec<IndividualPointer> = vec![];
 
-        pop.push(IndividualPointer::new(RefCell::<Individual>::new(
-            Individual::new(0),
-        )));
-        pop.push(IndividualPointer::new(RefCell::<Individual>::new(
-            Individual::new(1),
-        )));
+        pop.push(IndividualPointer::new(0));
+        pop.push(IndividualPointer::new(1));
 
         pop[0]
             .borrow_mut()
             .children
-            .insert(1, vec![Segment::new(0, 0, 2)]);
-        pop[1].borrow_mut().parents.push(pop[0].clone());
+            .insert(pop[1].clone(), vec![Segment::new(0, 0, 2)]);
+        pop[1].borrow_mut().parents.insert(pop[0].clone());
         assert_eq!(Rc::strong_count(&pop[0]), 2);
-        assert_eq!(Rc::strong_count(&pop[1]), 1);
+        assert_eq!(Rc::strong_count(&pop[1]), 2);
 
         remove_parent(pop[0].clone(), pop[1].clone());
         assert_eq!(Rc::strong_count(&pop[0]), 1);
-        assert_eq!(Rc::strong_count(&pop[1]), 1);
+        assert_eq!(Rc::strong_count(&pop[1]), 2);
     }
 
     #[test]
     fn test_interior_mutability_via_ref() {
         let mut pop: Vec<IndividualPointer> = vec![];
 
-        pop.push(IndividualPointer::new(RefCell::<Individual>::new(
-            Individual::new(0),
-        )));
-        pop.push(IndividualPointer::new(RefCell::<Individual>::new(
-            Individual::new(1),
-        )));
+        pop.push(IndividualPointer::new(0));
+        pop.push(IndividualPointer::new(1));
 
         pop[0]
             .borrow_mut()
             .children
-            .insert(1, vec![Segment::new(0, 0, 2)]);
-        pop[1].borrow_mut().parents.push(pop[0].clone());
+            .insert(pop[1].clone(), vec![Segment::new(0, 0, 2)]);
+        pop[1].borrow_mut().parents.insert(pop[0].clone());
         assert_eq!(Rc::strong_count(&pop[0]), 2);
-        assert_eq!(Rc::strong_count(&pop[1]), 1);
+        assert_eq!(Rc::strong_count(&pop[1]), 2);
 
         remove_parent_via_ref(&pop[0], &pop[1]);
         assert_eq!(Rc::strong_count(&pop[0]), 1);
-        assert_eq!(Rc::strong_count(&pop[1]), 1);
+        assert_eq!(Rc::strong_count(&pop[1]), 2);
     }
 
     #[test]
     fn test_interior_mutability_using_scoped_blocks() {
         let mut pop: Vec<IndividualPointer> = vec![];
 
-        pop.push(IndividualPointer::new(RefCell::<Individual>::new(
-            Individual::new(0),
-        )));
-        pop.push(IndividualPointer::new(RefCell::<Individual>::new(
-            Individual::new(1),
-        )));
+        pop.push(IndividualPointer::new(0));
+        pop.push(IndividualPointer::new(1));
 
         {
             let ind = &mut *pop[0].borrow_mut();
 
-            ind.children.insert(1, vec![Segment::new(0, 0, 2)]);
+            ind.children
+                .insert(pop[1].clone(), vec![Segment::new(0, 0, 2)]);
+        }
 
+        {
             let ind = &mut *pop[1].borrow_mut();
 
-            ind.parents.push(pop[0].clone());
+            ind.parents.insert(pop[0].clone());
         }
 
         assert_eq!(Rc::strong_count(&pop[0]), 2);
-        assert_eq!(Rc::strong_count(&pop[1]), 1);
+        assert_eq!(Rc::strong_count(&pop[1]), 2);
 
         remove_parent(pop[0].clone(), pop[1].clone());
         assert_eq!(Rc::strong_count(&pop[0]), 1);
-        assert_eq!(Rc::strong_count(&pop[1]), 1);
+        assert_eq!(Rc::strong_count(&pop[1]), 2);
     }
 }
