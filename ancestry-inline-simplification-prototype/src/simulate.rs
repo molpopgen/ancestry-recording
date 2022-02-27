ec9bd13c54803bea5144512029678c0f92728133
molpopgen <krthornt@uci.edu>
2022-02-27 09:51:51 -0800

    more sim setup

--- ancestry-inline-simplification-prototype/src/simulate.rs
+++ ancestry-inline-simplification-prototype/src/simulate.rs
@@ -1,12 +1,39 @@
+use crate::individual::Individual;
+use crate::population::Population;
 use crate::LargeSignedInteger;
 use crate::SignedInteger;
+use rand::distributions::Uniform;
 use rand::prelude::*;
 use rand_pcg::Pcg64;
 
-pub fn simulate(seed: u64, popsize: SignedInteger, genome_length: LargeSignedInteger) {
+pub struct SimParams {
+    popsize: SignedInteger,
+    num_steps: SignedInteger,
+    death_prob: f64,
+    genome_length: LargeSignedInteger,
+}
+
+pub fn simulate(seed: u64, params: SimParams) -> Population {
     let mut rng = Pcg64::seed_from_u64(seed);
 
-    let breakpoint = rand::distributions::Uniform::<LargeSignedInteger>::new(0, genome_length);
+    let breakpoint = Uniform::<LargeSignedInteger>::new(0, params.genome_length);
+    let u01 = Uniform::<f64>::new(0., 1.);
+    let pick_parent = Uniform::<SignedInteger>::new(0, params.popsize);
 
-    let _ = breakpoint.sample(&mut rng);
+    let mut pop = Population::new(params.popsize);
+
+    for birth_time in 1..(params.num_steps + 1) {
+        let mut deaths: Vec<usize> = vec![];
+        let mut replacements: Vec<Individual> = vec![];
+
+        // main loop here
+
+        assert_eq!(deaths.len(), replacements.len());
+
+        for (i, r) in deaths.iter().zip(replacements.iter_mut()) {
+            std::mem::swap(&mut pop.individuals[*i], &mut r.clone());
+        }
+    }
+
+    pop
 }
