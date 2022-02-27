4f541253b8b37f83cf1c027bb5caa6a108718db2
molpopgen <krthornt@uci.edu>
2022-02-27 08:12:38 -0800

    remove default test

--- ancestry-inline-simplification-prototype/src/lib.rs
+++ ancestry-inline-simplification-prototype/src/lib.rs
@@ -4,12 +4,3 @@ pub mod individual;
 pub mod population;
 pub mod simulate;
 pub mod segment;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
