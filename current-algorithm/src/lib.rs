b5d7e9e6b807a192525cf10d70d0edec35e7d0ce
molpopgen <krthornt@uci.edu>
2022-04-04 12:47:16 -0700

    remove unneeded crate

--- current-algorithm/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub use ancestry_common::{LargeSignedInteger, NodeFlags, SignedInteger};
-
-mod ancestry;
-mod simplify;
-
-pub use ancestry::{Ancestry, AncestryRecord, Segment};
-pub use simplify::simplify;
