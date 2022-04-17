e3d8e8febea5a79db3c6aa990198fa75f1b28e61
molpopgen <krthornt@uci.edu>
2022-04-17 15:22:55 -0700

    rename new file

--- ancestry-inline-simplification/src/lib.rs
+++ ancestry-inline-simplification/src/lib.rs
@@ -2,7 +2,7 @@ pub use ancestry_common::{LargeSignedInteger, SignedInteger};
 
 mod ancestry_overlapper;
 mod individual_heap;
-mod intervals;
+mod segments;
 
 pub mod individual;
 pub mod interval;
