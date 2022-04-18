9e2e122c784401dbf9ca622363637a9f634693ea
molpopgen <krthornt@uci.edu>
2022-04-18 15:23:18 -0700

    implement our evolve trait for Population

--- ancestry-inline-simplification/src/population.rs
+++ ancestry-inline-simplification/src/population.rs
@@ -2,10 +2,14 @@ use crate::individual::Individual;
 use crate::segments::HalfOpenInterval;
 use crate::LargeSignedInteger;
 use crate::SignedInteger;
+use neutral_evolution::NeutralEvolution;
 
 pub struct Population {
     next_individual_id: SignedInteger,
     genome_length: LargeSignedInteger,
+    replacements: Vec<usize>,
+    births: Vec<Individual>,
+    next_replacement: usize,
     pub individuals: Vec<Individual>,
 }
 
@@ -22,15 +26,18 @@ impl Population {
         Self {
             next_individual_id,
             genome_length,
+            replacements: vec![],
+            births: vec![],
+            next_replacement: 0,
             individuals,
         }
     }
 
-    pub fn birth(&mut self, birth_time: LargeSignedInteger) {
+    pub fn birth(&mut self, birth_time: LargeSignedInteger) -> Individual {
         assert!(birth_time >= 0);
         let index = self.next_individual_id;
         self.next_individual_id += 1;
-        Individual::new_alive(index, birth_time);
+        Individual::new_alive(index, birth_time)
     }
 
     pub fn get(&self, who: usize) -> Option<&Individual> {
@@ -63,5 +70,70 @@ impl Population {
 
     pub fn is_empty(&self) -> bool {
         self.individuals.is_empty()
+    }
+}
+
+impl NeutralEvolution for Population {
+    fn generate_deaths(&mut self, death: &mut neutral_evolution::Death) -> usize {
+        self.replacements.clear();
+        self.next_replacement = 0;
+
+        for i in 0..self.individuals.len() {
+            if death.dies() {
+                self.replacements.push(i);
+            }
+        }
+
+        self.replacements.len()
+    }
+
+    fn current_population_size(&self) -> usize {
+        self.individuals.len()
+    }
+
+    fn record_birth(
+        &mut self,
+        birth_time: LargeSignedInteger,
+        breakpoints: &[neutral_evolution::TransmittedSegment],
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let mut birth = self.birth(birth_time);
+
+        for b in breakpoints {
+            // Increase ref count of parent
+            let mut parent = self.get_mut(b.parent).as_mut().unwrap().clone();
+
+            // Add references to birth for each segment
+            parent.add_child_segment(b.left, b.right, birth.clone())?;
+            // MOVE parent w/o increasing ref count
+            birth.add_parent(parent)?;
+        }
+
+        // MOVE the birth w/o increasing ref count
+        self.births.push(birth);
+        Ok(())
+    }
+
+    fn simplify(
+        &mut self,
+        _current_time_point: LargeSignedInteger,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        assert_eq!(self.replacements.len(), self.births.len());
+
+        let ndeaths = self.replacements.len();
+
+        for death in 0..ndeaths {
+            let mut dead_ind = self.get(death).unwrap().clone();
+            self.kill(death);
+            dead_ind.propagate_upwards()?;
+            self.individuals[death] = self.births[death].clone();
+        }
+
+        for b in self.births.iter_mut() {
+            b.propagate_upwards()?;
+        }
+
+        self.births.clear();
+
+        Ok(())
     }
 }
