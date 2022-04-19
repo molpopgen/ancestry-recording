ac98b16d5e788dd34b968fe77d910477cbf98490
molpopgen <krthornt@uci.edu>
2022-04-19 11:22:26 -0700

    test of overlapping gens

--- tskit-evolution/tests/integration.rs
+++ tskit-evolution/tests/integration.rs
@@ -4,8 +4,29 @@ use tskit_evolution::*;
 #[test]
 fn test_simulation_round_trip() {
     for simplification_interval in 1..6_i64 {
-        let mut t = EvolvableTableCollection::new(tskit::Position::from(100.0), 10, simplification_interval).unwrap();
+        let mut t = EvolvableTableCollection::new(
+            tskit::Position::from(100.0),
+            10,
+            simplification_interval,
+        )
+        .unwrap();
         let p = Parameters::new(1.0, 1e-3, 100, 100).unwrap();
         evolve([101, 202], p, &mut t).unwrap();
+    }
+}
+
+#[test]
+fn test_simulation_round_trip_overlapping_gens() {
+    for pdeath in [0.25, 0.5, 0.75, 0.9] {
+        for simplification_interval in 1..6_i64 {
+            let mut t = EvolvableTableCollection::new(
+                tskit::Position::from(100.0),
+                10,
+                simplification_interval,
+            )
+            .unwrap();
+            let p = Parameters::new(pdeath, 1e-1, 100, 100).unwrap();
+            evolve([101, 202], p, &mut t).unwrap();
+        }
     }
 }
