f318508224769f55f9ff5300829449ee13067ded
molpopgen <krthornt@uci.edu>
2022-04-19 12:43:13 -0700

    clean new tests as we don't need simplification interval

--- ancestry-inline-simplification/tests/integration.rs
+++ ancestry-inline-simplification/tests/integration.rs
@@ -3,20 +3,16 @@ use neutral_evolution::{evolve, Parameters};
 
 #[test]
 fn test_simulation_round_trip() {
-    for simplification_interval in 1..6_i64 {
-        let mut pop = Population::new(10, 100);
-        let p = Parameters::new(1.0, 1e-3, 100, 100).unwrap();
-        evolve([101, 202], p, &mut pop).unwrap();
-    }
+    let mut pop = Population::new(10, 100);
+    let p = Parameters::new(1.0, 1e-3, 100, 100).unwrap();
+    evolve([101, 202], p, &mut pop).unwrap();
 }
 
 #[test]
 fn test_simulation_round_trip_overlapping_gens() {
     for pdeath in [0.25, 0.5, 0.75, 0.9] {
-        for simplification_interval in 1..6_i64 {
-            let mut pop = Population::new(10, 100);
-            let p = Parameters::new(pdeath, 1e-1, 100, 100).unwrap();
-            evolve([101, 202], p, &mut pop).unwrap();
-        }
+        let mut pop = Population::new(10, 100);
+        let p = Parameters::new(pdeath, 1e-1, 100, 100).unwrap();
+        evolve([101, 202], p, &mut pop).unwrap();
     }
 }
