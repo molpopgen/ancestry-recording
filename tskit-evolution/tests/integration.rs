a71f3567db9ef0baa8208d8fdc6eecc4e925f592
molpopgen <krthornt@uci.edu>
2022-04-19 10:14:37 -0700

    add a failing integration test

---
+++ tskit-evolution/tests/integration.rs
@@ -0,0 +1,11 @@
+use neutral_evolution::{evolve, Parameters};
+use tskit_evolution::*;
+
+#[test]
+fn test_simulation_round_trip() {
+    for simplification_interval in 1..6_i64 {
+        let mut t = EvolvableTableCollection::new(tskit::Position::from(100.0), 100, simplification_interval).unwrap();
+        let p = Parameters::new(1.0, 1e-3, 100, 100).unwrap();
+        evolve([101, 202], p, &mut t).unwrap();
+    }
+}
