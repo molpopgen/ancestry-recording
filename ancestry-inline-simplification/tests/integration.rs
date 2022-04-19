caf980e6a4ab281c07200f386f279a611a9d07f3
molpopgen <krthornt@uci.edu>
2022-04-19 12:52:28 -0700

    test tweaks

--- ancestry-inline-simplification/tests/integration.rs
+++ ancestry-inline-simplification/tests/integration.rs
@@ -3,9 +3,14 @@ use neutral_evolution::{evolve, Parameters};
 
 #[test]
 fn test_simulation_round_trip() {
+    // number of haploids, genome length
     let mut pop = Population::new(10, 100);
+    // death rate, mean no. crossovers, genome length, no. steps to sim
     let p = Parameters::new(1.0, 1e-3, 100, 100).unwrap();
     evolve([101, 202], p, &mut pop).unwrap();
+    for i in pop.individuals.iter() {
+        assert_eq!(i.borrow().birth_time, 100);
+    }
 }
 
 #[test]
