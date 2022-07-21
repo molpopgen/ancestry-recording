32529581394e76d6d41153c8fba7ebdfaae61765
Kevin R. Thornton <krthornt@uci.edu>
2022-07-21 15:28:10 -0700

    an idea for separating out the items

--- ancestry-inline-simplification/src/indexed_node.rs
+++ ancestry-inline-simplification/src/indexed_node.rs
@@ -23,6 +23,59 @@ pub struct Node {
     pub children: ChildMap,
 }
 
+#[derive(Default)]
+pub struct NodeTable {
+    pub index: Vec<usize>, // Redundant?
+    pub birth_time: Vec<LargeSignedInteger>,
+    pub flags: Vec<NodeFlags>,
+    pub parents: Vec<ParentSet>,
+    pub ancestry: Vec<Vec<AncestrySegment>>,
+    pub children: Vec<ChildMap>,
+    queue: Vec<usize>, // for recycling
+}
+
+impl NodeTable {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub(crate) fn new_birth(
+        &mut self,
+        birth_time: LargeSignedInteger,
+        genome_length: LargeSignedInteger,
+        parents: ParentSet,
+    ) -> usize {
+        match self.queue.pop() {
+            Some(index) => {
+                self.birth_time[index] = birth_time;
+                self.flags[index] = NodeFlags::new_alive();
+                self.parents[index] = parents;
+                self.ancestry[index].clear();
+                self.ancestry[index].push(AncestrySegment {
+                    segment: Segment::new(0, genome_length).unwrap(),
+                    child: index,
+                });
+                self.children[index].clear();
+
+                index
+            }
+            None => {
+                self.index.push(self.index.len());
+                self.birth_time.push(birth_time);
+                self.flags.push(NodeFlags::new_alive());
+                self.parents.push(parents);
+                self.ancestry.push(vec![AncestrySegment {
+                    segment: Segment::new(0, genome_length).unwrap(),
+                    child: self.index.len() - 1,
+                }]);
+                self.children.push(ChildMap::default());
+
+                self.index.len() - 1
+            }
+        }
+    }
+}
+
 impl Node {
     // FIXME: fallible b/c of Segment::new
     pub(crate) fn new_birth(
