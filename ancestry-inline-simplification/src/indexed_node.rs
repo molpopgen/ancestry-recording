4e2e2e2780d82e343a6a1ec6d24c9806df2e8029
Kevin R. Thornton <krthornt@uci.edu>
2022-07-31 14:19:57 -0700

    kill the old codez

--- ancestry-inline-simplification/src/indexed_node.rs
+++ ancestry-inline-simplification/src/indexed_node.rs
@@ -10,19 +10,6 @@ pub struct AncestrySegment {
 pub type ChildMap = nohash_hasher::IntMap<usize, Vec<Segment>>;
 pub type ParentSet = nohash_hasher::IntSet<usize>;
 
-// NOTE: this is probably borrow-checker hell waiting to happen!
-// This stuff will need to be broken up into several Vec<_>.
-// #[derive(Default)]
-// pub struct Node {
-//     /// Index of this node in the container
-//     pub index: usize,
-//     pub birth_time: LargeSignedInteger,
-//     pub flags: NodeFlags,
-//     pub parents: ParentSet,
-//     pub ancestry: Vec<AncestrySegment>,
-//     pub children: ChildMap,
-// }
-
 #[derive(Default)]
 pub struct NodeTable {
     pub index: Vec<usize>, // Redundant?
@@ -82,43 +69,3 @@ impl NodeTable {
         }
     }
 }
-
-//impl Node {
-//    // FIXME: fallible b/c of Segment::new
-//    pub(crate) fn new_birth(
-//        index: usize,
-//        birth_time: LargeSignedInteger,
-//        genome_length: LargeSignedInteger,
-//        parents: ParentSet,
-//    ) -> Self {
-//        Self {
-//            index,
-//            birth_time,
-//            flags: NodeFlags::new_alive(),
-//            parents,
-//            ancestry: vec![AncestrySegment {
-//                segment: Segment::new(0, genome_length).unwrap(),
-//                child: index,
-//            }],
-//            children: ChildMap::default(),
-//        }
-//    }
-//
-//    // FIXME: fallible b/c of Segment::new
-//    pub(crate) fn recycle(
-//        &mut self,
-//        birth_time: LargeSignedInteger,
-//        genome_length: LargeSignedInteger,
-//        parents: ParentSet,
-//    ) {
-//        self.ancestry.clear();
-//        self.children.clear();
-//        let mut p = parents;
-//        std::mem::swap(&mut self.parents, &mut p);
-//        self.ancestry.push(AncestrySegment {
-//            segment: Segment::new(0, genome_length).unwrap(),
-//            child: self.index,
-//        });
-//        self.birth_time = birth_time;
-//    }
-//}
