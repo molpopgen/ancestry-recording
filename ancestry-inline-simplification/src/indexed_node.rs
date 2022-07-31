2268f458a55355f477e0290eacab0100c269a2a3
Kevin R. Thornton <krthornt@uci.edu>
2022-07-31 14:17:36 -0700

    comment out the old design

--- ancestry-inline-simplification/src/indexed_node.rs
+++ ancestry-inline-simplification/src/indexed_node.rs
@@ -12,16 +12,16 @@ pub type ParentSet = nohash_hasher::IntSet<usize>;
 
 // NOTE: this is probably borrow-checker hell waiting to happen!
 // This stuff will need to be broken up into several Vec<_>.
-#[derive(Default)]
-pub struct Node {
-    /// Index of this node in the container
-    pub index: usize,
-    pub birth_time: LargeSignedInteger,
-    pub flags: NodeFlags,
-    pub parents: ParentSet,
-    pub ancestry: Vec<AncestrySegment>,
-    pub children: ChildMap,
-}
+// #[derive(Default)]
+// pub struct Node {
+//     /// Index of this node in the container
+//     pub index: usize,
+//     pub birth_time: LargeSignedInteger,
+//     pub flags: NodeFlags,
+//     pub parents: ParentSet,
+//     pub ancestry: Vec<AncestrySegment>,
+//     pub children: ChildMap,
+// }
 
 #[derive(Default)]
 pub struct NodeTable {
@@ -83,42 +83,42 @@ impl NodeTable {
     }
 }
 
-impl Node {
-    // FIXME: fallible b/c of Segment::new
-    pub(crate) fn new_birth(
-        index: usize,
-        birth_time: LargeSignedInteger,
-        genome_length: LargeSignedInteger,
-        parents: ParentSet,
-    ) -> Self {
-        Self {
-            index,
-            birth_time,
-            flags: NodeFlags::new_alive(),
-            parents,
-            ancestry: vec![AncestrySegment {
-                segment: Segment::new(0, genome_length).unwrap(),
-                child: index,
-            }],
-            children: ChildMap::default(),
-        }
-    }
-
-    // FIXME: fallible b/c of Segment::new
-    pub(crate) fn recycle(
-        &mut self,
-        birth_time: LargeSignedInteger,
-        genome_length: LargeSignedInteger,
-        parents: ParentSet,
-    ) {
-        self.ancestry.clear();
-        self.children.clear();
-        let mut p = parents;
-        std::mem::swap(&mut self.parents, &mut p);
-        self.ancestry.push(AncestrySegment {
-            segment: Segment::new(0, genome_length).unwrap(),
-            child: self.index,
-        });
-        self.birth_time = birth_time;
-    }
-}
+//impl Node {
+//    // FIXME: fallible b/c of Segment::new
+//    pub(crate) fn new_birth(
+//        index: usize,
+//        birth_time: LargeSignedInteger,
+//        genome_length: LargeSignedInteger,
+//        parents: ParentSet,
+//    ) -> Self {
+//        Self {
+//            index,
+//            birth_time,
+//            flags: NodeFlags::new_alive(),
+//            parents,
+//            ancestry: vec![AncestrySegment {
+//                segment: Segment::new(0, genome_length).unwrap(),
+//                child: index,
+//            }],
+//            children: ChildMap::default(),
+//        }
+//    }
+//
+//    // FIXME: fallible b/c of Segment::new
+//    pub(crate) fn recycle(
+//        &mut self,
+//        birth_time: LargeSignedInteger,
+//        genome_length: LargeSignedInteger,
+//        parents: ParentSet,
+//    ) {
+//        self.ancestry.clear();
+//        self.children.clear();
+//        let mut p = parents;
+//        std::mem::swap(&mut self.parents, &mut p);
+//        self.ancestry.push(AncestrySegment {
+//            segment: Segment::new(0, genome_length).unwrap(),
+//            child: self.index,
+//        });
+//        self.birth_time = birth_time;
+//    }
+//}
